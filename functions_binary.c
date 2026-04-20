8d3ddccf351be5f3fb27bd6ac9ead61475567360
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:16:05 +0200

    refactor convert_binary function to accept print_buffer_t argument for improved output handling and update documentation for clarity

--- functions_binary.c
+++ functions_binary.c
@@ -2,34 +2,36 @@
 
 /**
  * convert_binary - convert unsigned int in binary
- * @ap: print
+ * @ap: argument list
+ * @pb: output buffer
  *
- * Return: 0
+ * Return: number of characters printed, or -1 on error
  */
-int convert_binary(va_list ap)
+int convert_binary(va_list ap, print_buffer_t *pb)
 {
 	unsigned int n, max, i, sum;
 	unsigned int bin[32];
 	int len;
 
 	n = va_arg(ap, unsigned int);
-	/* (2 ^ 31), biggest positive number that fits in 32 bits*/
 	max = 2147483648;
 	bin[0] = n / max;
 	for (i = 1; i < 32; i++)
 	{
-		max /= 2;
+		max = max / 2;
 		bin[i] = (n / max) % 2;
 	}
-	for (i = 0, sum = 0, len = 0; i < 32; i++)
+	sum = 0;
+	len = 0;
+	for (i = 0; i < 32; i++)
 	{
-		sum += bin[i];
-		if (sum || i == 31)
+		sum = sum + bin[i];
+		if (sum != 0 || i == 31)
 		{
-			_putchar('0' + bin[i]);
-			len++;
+			if (pb_putc(pb, (char)('0' + bin[i])) < 0)
+				return (-1);
+			len = len + 1;
 		}
 	}
 	return (len);
 }
-

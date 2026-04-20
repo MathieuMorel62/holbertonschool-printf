64c4d264ff11dbd5d14feca7dd68ed7a92872974
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:17:22 +0200

    refactor print_pct function to accept print_buffer_t argument for improved output handling, update documentation to clarify unused parameters and error return value

--- print_percent_handler.c
+++ print_percent_handler.c
@@ -2,12 +2,14 @@
 
 /**
  * print_pct - prints the '%' character
- * @ap: argument list
+ * @ap: argument list (unused)
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_pct(va_list ap __attribute__((unused)))
+int print_pct(va_list ap __attribute__((unused)), print_buffer_t *pb)
 {
-	_putchar('%');
+	if (pb_putc(pb, '%') < 0)
+		return (-1);
 	return (1);
 }

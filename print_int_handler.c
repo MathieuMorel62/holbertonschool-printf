3c4985c46b22032387f3e04a549d8da8bbbd1018
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:17:15 +0200

    refactor print_int function to accept print_buffer_t argument for improved output handling, update documentation, and enhance error management

--- print_int_handler.c
+++ print_int_handler.c
@@ -1,34 +1,46 @@
 #include "main.h"
 
 /**
- * print_int - function to print integers
+ * print_int - print a signed integer in decimal
  * @ap: argument list
+ * @pb: output buffer
  *
- * Return: number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
-int print_int(va_list ap)
+int print_int(va_list ap, print_buffer_t *pb)
 {
-	int i, divisor = 1, result = 0;
-	unsigned int number;
+	int value;
+	unsigned int magnitude;
+	int divisor;
+	int char_to_print;
+	int count;
 
-	i = va_arg(ap, int);
+	value = va_arg(ap, int);
+	count = 0;
 
-	if (i < 0)
+	if (value < 0)
 	{
-		result += _putchar('-');
-		number = i * -1;
+		if (pb_putc(pb, '-') < 0)
+			return (-1);
+		count = count + 1;
+		magnitude = (unsigned int)(-(long)value);
 	}
 	else
-		number = i;
+		magnitude = (unsigned int)value;
 
-	while (number / divisor > 9)
-		divisor *= 10;
+	divisor = 1;
+	while ((magnitude / divisor) > 9)
+		divisor = divisor * 10;
 
 	while (divisor != 0)
 	{
-		result += _putchar(number / divisor + '0');
-		number %= divisor;
-		divisor /= 10;
+		char_to_print = (int)(magnitude / divisor) + (int)'0';
+		if (pb_putc(pb, (char)char_to_print) < 0)
+			return (-1);
+		count = count + 1;
+		magnitude = magnitude % divisor;
+		divisor = divisor / 10;
 	}
-	return (result);
+
+	return (count);
 }

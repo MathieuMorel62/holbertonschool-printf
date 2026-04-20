4dfa241862116a56225d14ddc7820c1577f19ae6
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-04-20 23:16:43 +0200

    add print_buffer struct and update function prototypes to utilize it for improved output management in _printf

--- main.h
+++ main.h
@@ -7,6 +7,17 @@
 #include <stdlib.h>
 
 /**
+ * struct print_buffer - local output buffer for _printf
+ * @buf: character storage (1024 bytes)
+ * @used: number of valid bytes in buf
+ */
+typedef struct print_buffer
+{
+	char buf[1024];
+	int used;
+} print_buffer_t;
+
+/**
  * struct print - type structure
  * @parameter: argument pointer
  * @f: pointer function to argument
@@ -15,22 +26,24 @@
 typedef struct print
 {
 	char *parameter;
-	int (*f)(va_list ap);
+	int (*f)(va_list ap, print_buffer_t *pb);
 } print_type;
 
 /* different prototypes */
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list ap);
-int print_string(va_list ap);
-int print_pct(va_list ap);
-int print_int(va_list ap);
-int convert_binary(va_list ap);
-int (*get_function(const char *format))(va_list);
-int print_unsigned(va_list ap);
-int print_octal(va_list ap);
-int print_hex_lower(va_list ap);
-int print_hex_upper(va_list ap);
+int pb_putc(print_buffer_t *pb, char c);
+int pb_flush(print_buffer_t *pb);
+int print_char(va_list ap, print_buffer_t *pb);
+int print_string(va_list ap, print_buffer_t *pb);
+int print_pct(va_list ap, print_buffer_t *pb);
+int print_int(va_list ap, print_buffer_t *pb);
+int convert_binary(va_list ap, print_buffer_t *pb);
+int (*get_function(const char *format))(va_list, print_buffer_t *);
+int print_unsigned(va_list ap, print_buffer_t *pb);
+int print_octal(va_list ap, print_buffer_t *pb);
+int print_hex_lower(va_list ap, print_buffer_t *pb);
+int print_hex_upper(va_list ap, print_buffer_t *pb);
 
 #endif
